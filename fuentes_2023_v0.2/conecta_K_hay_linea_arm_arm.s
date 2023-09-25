9d0304d168790bf1fd4c4c6284f7a997fdda745b
pabloangusto <62599902+pabloangusto@users.noreply.github.com>
2023-09-25 20:01:23 +0200

    ns q es esto

--- fuentes_2023_v0.2/conecta_K_hay_linea_arm_arm.s
+++ fuentes_2023_v0.2/conecta_K_hay_linea_arm_arm.s
@@ -3,7 +3,6 @@
 	AREA codigo,CODE, READONLY
 	ENTRY
 	IMPORT conecta_K_buscar_alineamiento_arm
-	IMPORT tablero_buscar
 	EXPORT conecta_K_hay_linea_arm_arm
 	PRESERVE8
 
@@ -36,41 +35,7 @@ for add r0, r13, #0x00000008
 	mov r1,r7
 	str r0,[r13,#0X0004]
 	mov r0,r9
-conecta_K_buscar_alineamiento_arm
-		stmdb r13!, {r2-r12,r14} ; guarda en la pila los valores de los registros previos a la rutina
-    mov r10, r0 ; guarda los parametros a usar en registros usables
-    mov r4, r1
-    mov r5, r2
-    mov r11, r3
-    add r7, r13, #0x00000030 ;apuntar a la cima de la pila
-    ldmia r7, {r6-r7}
-
-    
-    mov r3, r11 ; comprobar si la celda es valida y del mismo color
-    mov r2, r5
-    mov r1, r4
-    mov r0, r10
-    bl tablero_buscar_color
-    cmp r0, #0x00000000
-    beq exito
-    mov r0,#0x00000000 ;return 0
-salto    ldmia r13!, {r2-r12,r14}
-    bx r14 ;sale de la rutina
-
-exito add r0, r4, r6 ;nueva_fila++
-    and r8, r0, #0x000000FF
-    add r0, r5, r7 ;nueva_columna++
-    and r9, r0, #0x000000FF
-    mov r3, r11
-    mov r2, r9
-    mov r1, r8
-    mov r0, r10
-    stmia r13, {r6-r7}
-    bl conecta_K_buscar_alineamiento_arm
-    add r0, r0, #0x00000001
-    and r0, r0, #0x000000FF
-    b salto
-	
+	bl conecta_K_buscar_alineamiento_arm
 	mov r6,r0
 	cmp r6,#0x00000004
 	blt salto1
@@ -105,40 +70,7 @@ salir_if add r0,r13,#0x00000008
 	and r1,r0,#0x000000FF
 	mov r3,r10 
 	mov r0,r9 
-conecta_K_buscar_alineamiento_arm0 stmdb r13!, {r2-r12,r14} ; guarda en la pila los valores de los registros previos a la rutina
-    mov r10, r0 ; guarda los parametros a usar en registros usables
-    mov r4, r1
-    mov r5, r2
-    mov r11, r3
-    add r7, r13, #0x00000030 ;apuntar a la cima de la pila
-    ldmia r7, {r6-r7}
-
-    
-    mov r3, r11 ; comprobar si la celda es valida y del mismo color
-    mov r2, r5
-    mov r1, r4
-    mov r0, r10
-    bl tablero_buscar_color
-    cmp r0, #0x00000000
-    beq exito
-    mov r0,#0x00000000 ;return 0
-salto0    ldmia r13!, {r2-r12,r14}
-    bx r14 ;sale de la rutina
-
-exito0 add r0, r4, r6 ;nueva_fila++
-    and r8, r0, #0x000000FF
-    add r0, r5, r7 ;nueva_columna++
-    and r9, r0, #0x000000FF
-    mov r3, r11
-    mov r2, r9
-    mov r1, r8
-    mov r0, r10
-    stmia r13, {r6-r7}
-    bl conecta_K_buscar_alineamiento_arm
-    add r0, r0, #0x00000001
-    and r0, r0, #0x000000FF
-    b salto
-	
+	bl conecta_K_buscar_alineamiento_arm
 	add r0,r0,r6
 	add r6,r0, #0x000000FF
 	cmp r6, #0x00000004
@@ -160,5 +92,5 @@ fin	add r4,r4,#0x00000001
 salto5	mov r0,r5
 	add r13,r13,#0x00000010
 	ldmia r13!, {r4-r10,r14}
-	
+	bx r14
 	END
--- fuentes_2023_v0.2/traduciones.s
+++ fuentes_2023_v0.2/traduciones.s
@@ -1,8 +1,8 @@
-//uint8_t conecta_K_buscar_alineamiento_arm(TABLERO *t, uint8_t
+//uint8_t conecta_k_buscar_alineamiento_arm(tablero *t, uint8_t
 //	fila, uint8_t columna, uint8_t color, int8_t delta_fila, int8_t
 //	delta_columna);
 // comprobar si la celda es valida y del mismo color
-		//if (tablero_buscar_color(t, fila, columna, color) != EXITO) {
+		//if (tablero_buscar_color(t, fila, columna, color) != exito) {
 
 		//	return 0;
 		//}
@@ -12,166 +12,380 @@
     //uint8_t nueva_columna = columna + delta_columna;
 
     // incrementar longitud y visitar celda vecina
-    //return 1 + conecta_K_buscar_alineamiento_c(t, nueva_fila, nueva_columna, color, delta_fila, delta_columna);
+    //return 1 + conecta_k_buscar_alineamiento_c(t, nueva_fila, nueva_columna, color, delta_fila, delta_columna);
 
 
-    ;PROLOGO
-    STMDB R13!, {R2-R12,R14} ; guarda en la pila los valores de los registros previos a la rutina
-    MOV R10,R0 ; guarda los parametros a usar en registros usables
-    MOV R4,R1
-    MOV R5,R2
-    MOV R11,R3
-    ADD R7,R13,#0x00000030 ;apuntar a la cima de la pila
-    LDMIA R7,{R6-R7}
+    ;prologo
+    stmdb r13!, {r2-r12,r14} ; guarda en la pila los valores de los registros previos a la rutina
+    mov r10,r0 ; guarda los parametros a usar en registros usables
+    mov r4,r1
+    mov r5,r2
+    mov r11,r3
+    add r7,r13,#0x00000030 ;apuntar a la cima de la pila
+    ldmia r7,{r6-r7}
 
     ;comprobar si la celda es valida y del mismo color
-      MOV R3,R11
-      MOV R2,R5
-      MOV R1,R4
-      MOV R0,R10
-      BL tablero_buscar_color
-    CMP R0,#0x00000000
-    BEQ exito
-    MOV R0,#0x00000000 ;return 0
-    LDMIA R13, {R2-R12,R14}
-    BX R14 ;sale de la rutina
+      mov r3,r11
+      mov r2,r5
+      mov r1,r4
+      mov r0,r10
+      
+    stmdb r13!,{r4-r8,r14}; prologo
+    mov   r6,r0
+    mov   r5,r1
+    mov   r7,r2
+    mov   r8,r3
+    mov   r0,r5
+
+    mov   r1,r0; bl    tablero_fila_valida(0x0000075c)
+    cmp   r1,#0x00000007
+    bge   0x00000770
+    mov   r0,#0x00000001
+
+    cmp   r0,#0x00000000
+    beq   0x000006d0; salta a return error
+    mov   r0,r7
+
+    mov   r1,r0; bl    tablero_columna_valida(0x00000740)
+    cmp   r1,#0x00000007
+    bge   0x00000754
+    mov   r0,#0x00000001
+
+    cmp   r0,#0x00000000
+    bne   0x000006dc
+    mvn   r0,#0x00000000; return error
+    ldmia r13!,{r4-r8,r14}
+    bx    r14
+    mov   r4,#0x00000000; col = 0
+    b     0x000006e8
+    add   r4,r4,#0x00000001
+    cmp   r4,#0x00000006
+    bcs   0x00000704
+    add   r0,r5,r5,lsl #1
+    add   r0,r6,r0,lsl #1
+    ldrb  r0,[r0,r4]
+    cmp   r0,r7
+    bne   0x000006e4
+    cmp   r4,#0x00000006
+    bne   0x00000714
+    add   r1,r5,r5,lsl #1; celda_color()
+    add   r2,r6,#0x0000002a
+    add   r1,r2,r1,lsl #1
+    ldrb  r0,[r1,r4]
+    and   r1,r0,#0x00000003
+    cmp   r1,r8
+    bne   0x00000738
+    mov   r0,#0x00000000
+    b     0x000006d4
+    mvn   r0,#0x00000000
+    b     0x000006d4
+    mov   r1,r0
+    cmp   r1,#0x00000007
+    bge   0x00000754
+    mov   r0,#0x00000001
+    bx    r14
+
+    cmp r0,#0x00000000
+    beq exito
+    mov r0,#0x00000000 ;return 0
+    ldmia r13, {r2-r12,r14}
+    bx r14 ;sale de la rutina
 
   exito 
-    ADD R0,R4,R6 ;nueva_fila++
-    AND R8,R0,#0x000000FF
-    ADD R0,R5,R7 ;nueva_columna++
-    AND R9,R0,0x000000FF
-    MOV R3,R11
-    MOV R2,R9
-    MOV R1,R8
-    MOV R0,R10
-    STMIA R13, {R6-R7}
-    BL conecta_K_buscar_alineamiento_arm
-    ADD R0,R0,#0x00000001
-    AND R0,R0,#0x000000FF
-    B
+    add r0,r4,r6 ;nueva_fila++
+    and r8,r0,#0x000000ff
+    add r0,r5,r7 ;nueva_columna++
+    and r9,r0,0x000000ff
+    mov r3,r11
+    mov r2,r9
+    mov r1,r8
+    mov r0,r10
+    stmia r13, {r6-r7}
+    stmdb r13!,{r4-r8,r14}; prologo
+    mov   r6,r0
+    mov   r5,r1
+    mov   r7,r2
+    mov   r8,r3
+    mov   r0,r5
+
+    mov   r1,r0; bl    tablero_fila_valida(0x0000075c)
+    cmp   r1,#0x00000007
+    bge   0x00000770
+    mov   r0,#0x00000001
+
+    cmp   r0,#0x00000000
+    beq   0x000006d0; salta a return error
+    mov   r0,r7
+
+    mov   r1,r0; bl    tablero_columna_valida(0x00000740)
+    cmp   r1,#0x00000007
+    bge   0x00000754
+    mov   r0,#0x00000001
+
+    cmp   r0,#0x00000000
+    bne   0x000006dc
+    mvn   r0,#0x00000000; return error
+    ldmia r13!,{r4-r8,r14}
+    bx    r14
+    mov   r4,#0x00000000; col = 0
+    b     0x000006e8
+    add   r4,r4,#0x00000001
+    cmp   r4,#0x00000006
+    bcs   0x00000704
+    add   r0,r5,r5,lsl #1
+    add   r0,r6,r0,lsl #1
+    ldrb  r0,[r0,r4]
+    cmp   r0,r7
+    bne   0x000006e4
+    cmp   r4,#0x00000006
+    bne   0x00000714
+    add   r1,r5,r5,lsl #1; celda_color()
+    add   r2,r6,#0x0000002a
+    add   r1,r2,r1,lsl #1
+    ldrb  r0,[r1,r4]
+    and   r1,r0,#0x00000003
+    cmp   r1,r8
+    bne   0x00000738
+    mov   r0,#0x00000000
+    b     0x000006d4
+    mvn   r0,#0x00000000
+    b     0x000006d4
+    mov   r1,r0
+    cmp   r1,#0x00000007
+    bge   0x00000754
+    mov   r0,#0x00000001
+    bx    r14
+    add r0,r0,#0x00000001
+    and r0,r0,#0x000000ff
+    
     
 
 
 
 
-// devuelve true si encuentra una línea de longitud mayor o igual a _K
+// devuelve true si encuentra una línea de longitud mayor o igual a _k
 //uint8_t
-//conecta_K_hay_linea_arm_c(TABLERO *t, uint8_t fila, uint8_t columna, uint8_t color)
+//conecta_k_hay_linea_arm_c(tablero *t, uint8_t fila, uint8_t columna, uint8_t color)
 //{
-//	 enum { N_DELTAS = 4};
-//   int8_t deltas_fila[N_DELTAS] = {0, -1, -1, 1};
-//   int8_t deltas_columna[N_DELTAS] = {-1, 0, -1, -1};
+//	 enum { n_deltas = 4};
+//   int8_t deltas_fila[n_deltas] = {0, -1, -1, 1};
+//   int8_t deltas_columna[n_deltas] = {-1, 0, -1, -1};
 //   unsigned int i = 0;
-//   uint8_t linea = FALSE;
+//   uint8_t linea = false;
 //   uint8_t long_linea = 0;
 //
 //    buscar linea en fila, columna y 2 diagonales
-//   for(i=0; (i < N_DELTAS) && (linea == FALSE); ++i) {
+//   for(i=0; (i < n_deltas) && (linea == false); ++i) {
 //       // buscar sentido
-//       long_linea = conecta_K_buscar_alineamiento_c(t, fila, columna, color, deltas_fila[i], deltas_columna[i]);
-//       linea = long_linea >= K_SIZE;
+//       long_linea = conecta_k_buscar_alineamiento_c(t, fila, columna, color, deltas_fila[i], deltas_columna[i]);
+//       linea = long_linea >= k_size;
 //       if (linea) {
 //         continue;
 //       }
 //       // buscar sentido inverso
-//       long_linea += conecta_K_buscar_alineamiento_c(t, fila-deltas_fila[i],
+//       long_linea += conecta_k_buscar_alineamiento_c(t, fila-deltas_fila[i],
 //	       columna-deltas_columna[i], color, -deltas_fila[i], -deltas_columna[i]);
-//       linea = long_linea >= K_SIZE;
+//       linea = long_linea >= k_size;
 //   }
 //   return linea;
 //}
 
 
-STMDB R13!, {R0-R10,R14}
-MOV R9,R0
-MOV R7,R1
-MOV R8,R2
-MOV R10,R3
-ADD R0, PC, #0x0000011C
-LDR R0, [R0]
-STR R0,[R13,0x000C]
-ADD R0,PC,#0x00000114
-LDR R0,[R0]
-STR R0,[R13,#0x0008]
-MOV R4,#0x00000000
-MOV R5,#0x00000000
-MOV R6,#0x00000000
-NOP
-B fin
+stmdb r13!, {r0-r10,r14}
+mov r9,r0
+mov r7,r1
+mov r8,r2
+mov r10,r3
+add r0, pc, #0x0000011c
+ldr r0, [r0]
+str r0,[r13,0x000c]
+add r0,pc,#0x00000114
+ldr r0,[r0]
+str r0,[r13,#0x0008]
+mov r4,#0x00000000
+mov r5,#0x00000000
+mov r6,#0x00000000
+nop
+b fin
 
 for
-ADD R0,R13,#0x00000008
-LDRSB R0,[R0,R4]
-ADD R1,R13,#0x0000000C
-LDRSB R1,[R1,R4]
-MOV R3,R10
-MOV R2,R8
-STR R1,[R13]
-MOV R1,R7
-STR R0,[R13,#0X0004]
-MOV R0,R9
-BL conecta_K_buscar_alineamiento_c
-MOV R6,R0
-CMP R6,#0x00000004
-BLT salto1
-MOV R0,#0x00000001
-B salto2
+add r0,r13,#0x00000008
+ldrsb r0,[r0,r4]
+add r1,r13,#0x0000000c
+ldrsb r1,[r1,r4]
+mov r3,r10
+mov r2,r8
+str r1,[r13]
+mov r1,r7
+str r0,[r13,#0x0004]
+mov r0,r9
+stmdb r13!,{r4-r8,r14}; prologo
+mov   r6,r0
+mov   r5,r1
+mov   r7,r2
+mov   r8,r3
+mov   r0,r5
+
+mov   r1,r0; bl    tablero_fila_valida(0x0000075c)
+cmp   r1,#0x00000007
+bge   0x00000770
+mov   r0,#0x00000001
+
+cmp   r0,#0x00000000
+beq   0x000006d0; salta a return error
+mov   r0,r7
+
+mov   r1,r0; bl    tablero_columna_valida(0x00000740)
+cmp   r1,#0x00000007
+bge   0x00000754
+mov   r0,#0x00000001
+
+cmp   r0,#0x00000000
+bne   0x000006dc
+mvn   r0,#0x00000000; return error
+ldmia r13!,{r4-r8,r14}
+bx    r14
+mov   r4,#0x00000000; col = 0
+b     0x000006e8
+add   r4,r4,#0x00000001
+cmp   r4,#0x00000006
+bcs   0x00000704
+add   r0,r5,r5,lsl #1
+add   r0,r6,r0,lsl #1
+ldrb  r0,[r0,r4]
+cmp   r0,r7
+bne   0x000006e4
+cmp   r4,#0x00000006
+bne   0x00000714
+add   r1,r5,r5,lsl #1; celda_color()
+add   r2,r6,#0x0000002a
+add   r1,r2,r1,lsl #1
+ldrb  r0,[r1,r4]
+and   r1,r0,#0x00000003
+cmp   r1,r8
+bne   0x00000738
+mov   r0,#0x00000000
+b     0x000006d4
+mvn   r0,#0x00000000
+b     0x000006d4
+mov   r1,r0
+cmp   r1,#0x00000007
+bge   0x00000754
+mov   r0,#0x00000001
+bx    r14
+mov r6,r0
+cmp r6,#0x00000004
+blt salto1
+mov r0,#0x00000001
+b salto2
 
 salto1
-MOV R0,#0x00000000
+mov r0,#0x00000000
 
 salto2
-MOV R5,R0
-CMP R5,#0x00000000
-BEQ salir_if
-B fin
+mov r5,r0
+cmp r5,#0x00000000
+beq salir_if
+b fin
 salir_if 
-ADD R0,R13,#0x00000008
-LDRB R0,[R0,R4]
-RSB R0,R0,#0x00000000
-MOV R0,R0,LSL #24
-MOV R0,R0,ASR #24
-ADD R1,R13,#0x0000000C
-LDRB R1,[R1,R4]
-RSB R1,R1,#0x00000000
-MOV R0,R0,LSL #24
-MOV R0,R0,ASR #24
-STR R0,[R13,#0X0004]
-STR R1,[R13]
-ADD R0,R13, #0x00000008
-LDRB R0,[R0,R4]
-SUB R0,R8,R0 
-AND R2,R0,#0x000000FF
-ADD R0,R12,#0x0000000C
-LDRB R0,[R0,R4]
-SUB R0,R7,R0  
-AND R1,R0,#0x000000FF
-MOV R3,R10 
-MOV R0,R9 
-BL conecta_K_buscar_alineamiento_c
-ADD R0,R0,R6
-ADD R6,R0, #0x000000FF
-CMP R6, #0x00000004
-BLT salto3
-MOV R0,#0x00000001
-B salto4
+add r0,r13,#0x00000008
+ldrb r0,[r0,r4]
+rsb r0,r0,#0x00000000
+mov r0,r0,lsl #24
+mov r0,r0,asr #24
+add r1,r13,#0x0000000c
+ldrb r1,[r1,r4]
+rsb r1,r1,#0x00000000
+mov r0,r0,lsl #24
+mov r0,r0,asr #24
+str r0,[r13,#0x0004]
+str r1,[r13]
+add r0,r13, #0x00000008
+ldrb r0,[r0,r4]
+sub r0,r8,r0 
+and r2,r0,#0x000000ff
+add r0,r12,#0x0000000c
+ldrb r0,[r0,r4]
+sub r0,r7,r0  
+and r1,r0,#0x000000ff
+mov r3,r10 
+mov r0,r9 
+stmdb r13!,{r4-r8,r14}; prologo
+mov   r6,r0
+mov   r5,r1
+mov   r7,r2
+mov   r8,r3
+mov   r0,r5
+
+mov   r1,r0; bl    tablero_fila_valida(0x0000075c)
+cmp   r1,#0x00000007
+bge   0x00000770
+mov   r0,#0x00000001
+
+cmp   r0,#0x00000000
+beq   0x000006d0; salta a return error
+mov   r0,r7
+
+mov   r1,r0; bl    tablero_columna_valida(0x00000740)
+cmp   r1,#0x00000007
+bge   0x00000754
+mov   r0,#0x00000001
+
+cmp   r0,#0x00000000
+bne   0x000006dc
+mvn   r0,#0x00000000; return error
+ldmia r13!,{r4-r8,r14}
+bx    r14
+mov   r4,#0x00000000; col = 0
+b     0x000006e8
+add   r4,r4,#0x00000001
+cmp   r4,#0x00000006
+bcs   0x00000704
+add   r0,r5,r5,lsl #1
+add   r0,r6,r0,lsl #1
+ldrb  r0,[r0,r4]
+cmp   r0,r7
+bne   0x000006e4
+cmp   r4,#0x00000006
+bne   0x00000714
+add   r1,r5,r5,lsl #1; celda_color()
+add   r2,r6,#0x0000002a
+add   r1,r2,r1,lsl #1
+ldrb  r0,[r1,r4]
+and   r1,r0,#0x00000003
+cmp   r1,r8
+bne   0x00000738
+mov   r0,#0x00000000
+b     0x000006d4
+mvn   r0,#0x00000000
+b     0x000006d4
+mov   r1,r0
+cmp   r1,#0x00000007
+bge   0x00000754
+mov   r0,#0x00000001
+bx    r14
+add r0,r0,r6
+add r6,r0, #0x000000ff
+cmp r6, #0x00000004
+blt salto3
+mov r0,#0x00000001
+b salto4
 
 salto3
-MOV R0,#0x00000000
+mov r0,#0x00000000
 
 salto4
-MOV R5,R0
-NOP 
+mov r5,r0
+nop 
 
 fin
-ADD R4,R4,#0x00000001
-CMP R4,#0x00000004
-BCS salto5
-CMP R5,#0x00000000
-BEQ for
+add r4,r4,#0x00000001
+cmp r4,#0x00000004
+bcs salto5
+cmp r5,#0x00000000
+beq for
 
 salto5
-MOV R0,R5
-ADD R13,R13,#0x00000010
-LDMIA R13!, {R4-R10,R14}
+mov r0,r5
+add r13,r13,#0x00000010
+ldmia r13!, {r4-r10,r14}
