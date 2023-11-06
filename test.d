63018454551a4a1cb3fa3497e2afb1f00970f35e
pabloangusto <62599902+pabloangusto@users.noreply.github.com>
2023-11-06 21:14:18 +0100

    Add files via upload

---
+++ test.d
@@ -0,0 +1,9 @@
+.\test.o: test.c
+.\test.o: test.h
+.\test.o: alarmas.h
+.\test.o: fifo.h
+.\test.o: D:\keil\ARM\ARMCC\Bin\..\include\inttypes.h
+.\test.o: D:\keil\ARM\ARMCC\Bin\..\include\stdint.h
+.\test.o: gpio_hal.h
+.\test.o: D:\keil\ARM\INC\Philips\LPC210X.H
+.\test.o: io_reserva.h
---
+++ visualizar.h
@@ -0,0 +1,7 @@
+
+#include "gpio_hal.h"
+
+void visualizarInicializar(void);
+void visualizarCuenta(uint32_t cuenta);
+void visualizarHello(uint32_t led);
+
